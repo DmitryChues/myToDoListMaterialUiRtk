bbc73927bc0de6921baae024103b552b4285c2bd
DmitryChues <dmitrychues2312@gmail.com>
2024-09-13 21:06:00 +0300

    rewrite store with use configureStore

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,45 +1,37 @@
-import { AuthActionsType, authReducer } from 'features/Login/authReducer'
+import {
+  combineReducers,
+  configureStore,
+  UnknownAction,
+} from '@reduxjs/toolkit'
+import { authSlice } from 'features/Login/authReducer'
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
-import { applyMiddleware, combineReducers, legacy_createStore } from 'redux'
-import { thunk, ThunkAction, ThunkDispatch } from 'redux-thunk'
-import {
-  TaskActionsType,
-  tasksReducer,
-} from '../features/TodolistsList/tasksReducer'
-import {
-  TodolistActionsType,
-  todolistReducer,
-} from '../features/TodolistsList/todolistsReducer'
-import { AppActionsType, appReducer } from './appReducer'
+import { ThunkAction, ThunkDispatch } from 'redux-thunk'
+import { tasksReducer } from '../features/TodolistsList/tasksReducer'
+import { todolistReducer } from '../features/TodolistsList/todolistsReducer'
+import { appReducer } from './appReducer'
 
 const rootReducer = combineReducers({
   tasks: tasksReducer,
   todolist: todolistReducer,
   app: appReducer,
-  auth: authReducer,
+  [authSlice.reducerPath]: authSlice.reducer,
 })
 
-export const store = legacy_createStore(rootReducer, applyMiddleware(thunk))
+export const store = configureStore({ reducer: rootReducer })
 
-export type AppRootStateType = ReturnType<typeof rootReducer>
-
-export type CommonActionsType =
-  | TodolistActionsType
-  | TaskActionsType
-  | AuthActionsType
-  | AppActionsType
+export type AppRootStateType = ReturnType<typeof store.getState>
 
 export type AppThunkDispatch = ThunkDispatch<
   AppRootStateType,
   unknown,
-  CommonActionsType
+  UnknownAction
 >
 
 export type AppThunk<ReturnType = void> = ThunkAction<
   ReturnType,
   AppRootStateType,
   unknown,
-  CommonActionsType
+  UnknownAction
 >
 
 export const useAppDispatch = () => useDispatch<AppThunkDispatch>()
